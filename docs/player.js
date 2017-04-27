1a9224422e13e641d3479d05d2548eaf7a416db7
flowerlotus11 <kareren315@yahoo.co.jp>
2017-04-27 12:39:30 +0900

    03-janken

--- docs/player.js
+++ docs/player.js
@@ -1,3 +1,14 @@
+const GU = 0;
+
+let matches = 0;
+
 function action(oppornent){
-  return 1;
+  matches = matches + 1;
+  const amari = matches % 2;
+  const isEven = amari == 0;
+  console.log(`${matches}) % 2 = ${reminder} : ${isEven}`);
+  if(isEven){
+   return GU;
+  }
+  return CHOKI;
 }
